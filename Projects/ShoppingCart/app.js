dbe7b2e3406f1c0e4be5c11667e08ff807bb686a
Trebacz <Marcin.Trebacz@opitz-consulting.com>
2021-08-10 19:20:36 +0200

    add BUY functionality

--- Projects/ShoppingCart/app.js
+++ Projects/ShoppingCart/app.js
@@ -1,29 +1,38 @@
 class App extends React.Component {
     state = {
         availableProducts: 7,
-        shoppingCart: 5
+        shoppingCart: 0
     }
 
     handleRemoveFromCart = () => {
         this.setState({
-            shoppingCart: this.state.shoppingCart -1
+            shoppingCart: this.state.shoppingCart - 1
         })
     }
+
     handleAddToCart = () => {
         this.setState({
-            
-            shoppingCart: this.state.shoppingCart +1
+
+            shoppingCart: this.state.shoppingCart + 1
+        })
+    }
+
+    handleBuy = () => {
+        this.setState({
+            availableProducts: this.state.availableProducts - this.state.shoppingCart,
+            shoppingCart: 0
         })
     }
 
     render() {
         return (
             <>
-                <button disabled={this.state.shoppingCart ? false : true} 
-                onClick={this.handleRemoveFromCart}>-</button>
+                <button disabled={this.state.shoppingCart ? false : true}
+                    onClick={this.handleRemoveFromCart}>-</button>
                 <span> {this.state.shoppingCart} </span>
-                <button disabled={this.state.shoppingCart===this.state.availableProducts ? true : false}
-                onClick={this.handleAddToCart}>+</button>
+                <button disabled={this.state.shoppingCart === this.state.availableProducts ? true : false}
+                    onClick={this.handleAddToCart}>+</button>
+                {this.state.shoppingCart > 0 && <button onClick={this.handleBuy}>Buy</button>}
             </>
         )
     }
