7eacfeffb83adc16ef9c08ac7f2740d7e0bba881
Trebacz <Marcin.Trebacz@opitz-consulting.com>
2021-08-19 18:43:14 +0200

    using objects as data

--- Projects/Arrays/app.js
+++ Projects/Arrays/app.js
@@ -1,13 +1,45 @@
-const Item = (props) => <li> {`fruit: ${props.content}`} </li>
+const data = {
+    users: [
+        {
+            id: 1,
+            age: 29,
+            name: "John"
+        },
+        {
+            id: 2,
+            age: 39,
+            name: "Mark"
+        },
+        {
+            id: 3,
+            age: 19,
+            name: "Kate"
+        },
+    ]
+}
+
+/* const Person = (props) => (
+    <div>
+        <h1>User {props.user.name}</h1>
+        <h2>is {props.user.age}</h2>
+    </div>
+) */
+
+const Item = ({ user }) => ( // we take specific prop
+    <div>
+        <h1>User {user.name}</h1>
+        <h2>is {user.age}</h2>
+    </div>
+)
 
 class ListItems extends React.Component {
 
-    state = {
+    /* state = {
         items: ["apple", "pear", "banana"]
-    }
+    } */
     render() {
-
-        const Items = this.state.items.map(item => <Item key={item} content={item} />)
+        const users = this.props.data.users;
+        const Items = users.map(user => <Item key={user.id} user={user} />)
         return (
             <ul>
                 {/* {this.state.items.map(item => <Item key={item} content={item} />)} */}
@@ -17,4 +49,4 @@ class ListItems extends React.Component {
     }
 }
 
-ReactDOM.render(<ListItems />, document.getElementById("root"));
+ReactDOM.render(<ListItems data={data} />, document.getElementById("root"));
