57f1ab41c35df43cfb0d44ee682b21f0055a5b1c
Trebacz <Marcin.Trebacz@opitz-consulting.com>
2021-09-04 15:49:56 +0200

    ExchangeCounter - 'state' modified; array list created

--- Projects/ExchangeCounter/app.js
+++ Projects/ExchangeCounter/app.js
@@ -1,13 +1,3 @@
-/* const Dollars = (props) => (
-    <div> Value (Dollars): {props.cash <= 0 ? "" : (props.cash / props.ratio).toFixed(2)}</div>
-)
-const Euros = (props) => {
-    const value = (props.cash / props.ratio).toFixed(2)
-    return (
-        <div> Value (Euros): {props.cash <= 0 ? "" : value} </div>
-    )
-}
- */
 const Cash = (props) => {
     const value = (props.cash / props.ratio).toFixed(2)
     return (
@@ -15,14 +5,33 @@ const Cash = (props) => {
     )
 }
 
-
-
 class ExchangeCounter extends React.Component {
     state = {
         amount: "",
-        ratioDollar: 3.8,
-        ratioEuro: 4.2
+        /*         ratioDollar: 3.8,
+                ratioEuro: 4.2 */
     }
+
+    currencies = [
+        {
+            id: 1,
+            name: "dollar",
+            ratio: 3.8,
+            title: "Value (Dollars):"
+        },
+        {
+            id: 2,
+            name: "euro",
+            ratio: 4.3,
+            title: "Value (Euros):"
+        },
+        {
+            id: 3,
+            name: "pounds",
+            ratio: 5.2,
+            title: "Value (Pounds):"
+        },
+    ]
     hadleChange = (e) => {
         this.setState({
             amount: e.target.value
@@ -31,16 +40,19 @@ class ExchangeCounter extends React.Component {
 
     render() {
         const { amount, ratioDollar, ratioEuro } = this.state;
+
+        const calculators = this.currencies.map(curr => (
+            <Cash key={curr.id} ratio={curr.ratio} title={curr.title} cash={amount} />
+        ))
         return (
             <>
                 <div className="app">
                     <label>
                         <input type="number" value={this.state.amount} onChange={this.hadleChange} />
                     </label>
-                    {/* <Dollars cash={amount} ratio={ratioDollar} />
-                    <Euros cash={amount} ratio={ratioEuro} /> */}
-                    <Cash cash={amount} ratio={ratioDollar} title="Value (Dollars):" />
-                    <Cash cash={amount} ratio={ratioEuro} title="Value (Euro):" />
+                    {/*                     <Cash cash={amount} ratio={ratioDollar} title="Value (Dollars):" />
+                    <Cash cash={amount} ratio={ratioEuro} title="Value (Euro):" /> */}
+                    {calculators}
                 </div>
             </>
         )
